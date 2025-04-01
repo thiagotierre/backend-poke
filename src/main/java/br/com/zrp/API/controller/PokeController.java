c3b4a17579906a60b0f7262e04e3cd3e07d260c1
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-03-31 22:34:32 -0300

    configure sonarqube

--- src/main/java/br/com/zrp/API/controller/PokeController.java
+++ src/main/java/br/com/zrp/API/controller/PokeController.java
@@ -2,13 +2,13 @@ package br.com.zrp.API.controller;
 
 import br.com.zrp.API.model.Pokemon;
 import br.com.zrp.API.service.PokeService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -20,13 +20,21 @@ public class PokeController {
     private PokeService service;
 
     @GetMapping("/pokemon/{name}")
-    public ResponseEntity<Pokemon> getPokeByName(@PathVariable("name") String name) throws IOException {
+    public ResponseEntity<Pokemon> getPokeByName(@PathVariable("name") String name) {
         ObjectMapper mapper = new ObjectMapper();
+        Optional<Pokemon> pokemonOptional = Optional.empty();
 
-        Optional<Object> opt = Optional.of(service.getPokeByName(name))
-                .orElseGet(Optional::empty);
-        Optional<Pokemon> pokemonOptional = Optional
-                .of(Pokemon.fromJson(mapper.writeValueAsString(opt.get())));
+        try{
+            pokemonOptional = Optional.of(Optional
+                    .of(Pokemon.fromJson(mapper
+                            .writeValueAsString(service.getPokeByName(name))))
+                    .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND)));
+
+        } catch (HttpClientErrorException e) {
+            throw new RuntimeException(HttpStatus.NOT_FOUND.toString());
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
 
         return pokemonOptional
                 .map(pokemon -> ResponseEntity.ok().body(pokemon))
---
+++ src/test/java/br/com/zrp/API/service/PokeServiceTest.java
@@ -0,0 +1,74 @@
+package br.com.zrp.API.service;
+
+import br.com.zrp.API.controller.PokeController;
+import br.com.zrp.API.model.Pokemon;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class PokeServiceTest {
+
+    @InjectMocks
+    private PokeController pokeController;
+
+    @Mock
+    private PokeService pokeService;
+
+    @Test
+    void getPokeByName_ExistingPokemon_ReturnsOkAndPokemon() throws IOException {
+        // Arrange
+        String pokemonName = "Pikachu";
+        Pokemon mockPokemon = new Pokemon();
+        mockPokemon.setName(pokemonName);
+        // Simulate the behavior of the PokeService
+        when(pokeService.getPokeByName(pokemonName)).thenReturn(Optional.of(mockPokemon));
+
+        // Act
+        ResponseEntity<Pokemon> responseEntity = pokeController.getPokeByName(pokemonName);
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(pokemonName, responseEntity.getBody().getName());
+    }
+
+//    @Test
+//    void getPokeByName_NonExistingPokemon_ReturnsNotFound() throws IOException {
+//        // Arrange
+//        String pokemonName = "NonExistentPokemon";
+//        // Simulate the behavior of the PokeService to return null or throw an exception
+//        when(pokeService.getPokeByName(pokemonName)).thenReturn(null);
+//
+//        // Act
+//        ResponseEntity<Pokemon> responseEntity = pokeController.getPokeByName(pokemonName);
+//
+//        // Assert
+//        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+//        assertNull(responseEntity.getBody());
+//    }
+//
+//    @Test
+//    void getPokeByName_ServiceThrowsIOException_ReturnsNotFound() throws IOException {
+//        // Arrange
+//        String pokemonName = "ErrorPokemon";
+//        // Simulate the PokeService throwing an IOException
+//        when(pokeService.getPokeByName(pokemonName)).thenThrow(new IOException("Simulated IO Exception"));
+//
+//        // Act
+//        ResponseEntity<Pokemon> responseEntity = pokeController.getPokeByName(pokemonName);
+//
+//        // Assert
+//        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+//        assertNull(responseEntity.getBody());
+//    }
+}
